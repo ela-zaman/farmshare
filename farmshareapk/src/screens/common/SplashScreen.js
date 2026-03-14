aabdeaebdc1c5b3bf35a4469d304a14836cc63f8
Razia Zaman Ela <raziazaman@iut-dhaka.edu>
2026-03-14 11:55:39 +0600

    splash screen logo implemented

--- farmshareapk/src/screens/common/SplashScreen.js
+++ farmshareapk/src/screens/common/SplashScreen.js
@@ -1,35 +1,46 @@
 import React, { useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 
 export default function SplashScreen({ navigation }) {
 
   useEffect(() => {
 
-    setTimeout(() => {
-      navigation.replace("Login");
-    }, 2000);
+    const timer = setTimeout(() => {
+      navigation.reset({
+        index: 0,
+        routes: [{ name: "Login" }],
+      }); // go to intro screen
+    }, 3000); // show splash for 3 seconds
+
+    return () => clearTimeout(timer);
 
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>FarmShare</Text>
-      <Text>Loading...</Text>
+
+      <Image
+        source={require("G:/Farm-Share Ap/farmshare/farmshareapk/assets/logo/Logo1.png")}
+        style={styles.logo}
+      />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center",
-    backgroundColor:"#2E7D32"
+
+  container: {
+    flex: 1,
+    backgroundColor: "#2E7D32",
+    justifyContent: "center",
+    alignItems: "center"
   },
-  title:{
-    fontSize:32,
-    color:"white",
-    fontWeight:"bold",
-    marginBottom:10
+
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain"
   }
+
 });
