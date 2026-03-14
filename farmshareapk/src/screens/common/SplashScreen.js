e5ad3ec7b525c457b636efa426ab46b0691b08a6
Razia Zaman Ela <raziazaman@iut-dhaka.edu>
2026-03-14 12:06:55 +0600

    fly in logo implemented

--- farmshareapk/src/screens/common/SplashScreen.js
+++ farmshareapk/src/screens/common/SplashScreen.js
@@ -1,46 +1,63 @@
-import React, { useEffect } from "react";
-import { View, Image, StyleSheet } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { View, Image, StyleSheet, Animated, Dimensions } from "react-native";
+
+const { width } = Dimensions.get("window");
 
 export default function SplashScreen({ navigation }) {
 
+  const slideAnim = useRef(new Animated.Value(-width)).current; // start from left side
+  const opacityAnim = useRef(new Animated.Value(0)).current;
+
   useEffect(() => {
 
-    const timer = setTimeout(() => {
+    Animated.parallel([
+      Animated.timing(slideAnim, {
+        toValue: 0,
+        duration: 1200,
+        useNativeDriver: true
+      }),
+      Animated.timing(opacityAnim, {
+        toValue: 1,
+        duration: 1200,
+        useNativeDriver: true
+      })
+    ]).start();
+
+    setTimeout(() => {
       navigation.reset({
         index: 0,
         routes: [{ name: "Login" }],
-      }); // go to intro screen
-    }, 3000); // show splash for 3 seconds
-
-    return () => clearTimeout(timer);
+      }); 
+    }, 2500);
 
   }, []);
 
   return (
     <View style={styles.container}>
-
-      <Image
+      <Animated.Image
         source={require("G:/Farm-Share Ap/farmshare/farmshareapk/assets/logo/Logo1.png")}
-        style={styles.logo}
+        style={[
+          styles.logo,
+          {
+            transform: [{ translateX: slideAnim }],
+            opacity: opacityAnim
+          }
+        ]}
+        resizeMode="contain"
       />
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     backgroundColor: "#2E7D32",
     justifyContent: "center",
     alignItems: "center"
   },
-
   logo: {
     width: 200,
-    height: 200,
-    resizeMode: "contain"
+    height: 200
   }
-
 });
